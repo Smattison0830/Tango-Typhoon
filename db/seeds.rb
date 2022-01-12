8e68e9edefbcc735eb1789c6cfef28d3b11f0dcc
Spencer Mattison <spenceramattison@gmail.com>
2022-01-12 16:02:55 +0900

    created 8 seeds with english and japanese names, also added picture placeholder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Vocab.destroy_all
+User.destroy_all
+
+spencer = User.create!(
+  name: "Spencer",
+  email: "spencer@me.org",
+  password: "spencerlife",
+  line_id: "supenchan"
+)
+
+Vocab.create!(
+    english: "Apple"
+    japanese: "りんご"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Orange"
+    japanese: "オレンジ"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Banana"
+    japanese: "バナナ"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Kiwi Fruit"
+    japanese: "キウイ"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Strawberry"
+    japanese: "いちご"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Peach"
+    japanese: "もも"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Lemon"
+    japanese: "レモン"
+    picture: ""
+)
+
+Vocab.create!(
+    english: "Watermelon"
+    japanese: "すいか"
+    picture: ""
+)
