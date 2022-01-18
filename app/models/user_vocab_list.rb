a2f29b7409179814e85765cbccafbae2acf75c3b
Spencer Mattison <spenceramattison@gmail.com>
2022-01-18 21:14:09 +0900

    deleted user_vocab and user_vocab_list models, created cards model

--- app/models/user_vocab_list.rb
+++
@@ -1,2 +0,0 @@
-class UserVocabList < ApplicationRecord
-end
--- db/migrate/20220117041440_create_user_vocab_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserVocabLists < ActiveRecord::Migration[6.1]
-  def change
-    create_table :user_vocab_lists do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
