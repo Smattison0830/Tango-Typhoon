739ff70d5abade630aa8be8af67c1a94fec4fa73
Spencer Mattison <spenceramattison@gmail.com>
2022-01-21 16:21:51 +0900

    wrote initial routes for lists, vocabular and cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :lists, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  resources :vocabulary, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  resources :cards, only: [:index, :new, :create, :show, :destroy]
 end
