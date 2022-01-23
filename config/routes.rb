626546b3807f0b6e8a5697b66d244a24b340f3c8
Spencer Mattison <spenceramattison@gmail.com>
2022-01-23 11:57:55 +0900

    fixed routes. added vocab routes to lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :lists, only: [:index, :new, :create, :show, :destroy, :edit, :update]
-  resources :vocabulary, only: [:index, :new, :create, :show, :destroy, :edit, :update]
-  resources :cards, only: [:index, :new, :create, :show, :destroy]
+  resources :lists, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
+    resources :vocabulary, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  end
+  
+  resources :cards, path: "tango_typhoon", only: [:index, :new, :create, :show, :destroy]
 end
