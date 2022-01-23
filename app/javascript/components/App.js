82cc29c22ca0d25e9ef52205dcbbf32759b45f7f
Spencer Mattison <spenceramattison@gmail.com>
2022-01-23 18:44:16 +0900

    created simple react page and got it to render

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hellow world, this is your REACT App</h1>
+        )
+    }
+}
+export default App
