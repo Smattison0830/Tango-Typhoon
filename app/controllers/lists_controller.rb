7abae004e2f48043d2803971a759fbc25620624d
Spencer Mattison <spenceramattison@gmail.com>
2022-01-24 14:48:51 +0900

    installed pundit, edited application controller to use pundit.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,58 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: [:create, :edit, :update, :destroy]
+
+    def index
+      @lists = policy_scope(List).order(created_at: :desc)
+    end
+  
+    # def show; end
+  
+    # def water
+    #   @pot.update(pot_params)
+    #   redirect_to pot_path
+    # end
+  
+    def create
+      @list = List.new(list_params)
+    #   @plant = Plant.find(params[:pot][:plant_id])
+    #   @pot.plant = @plant
+      @list.user = current_user
+    #   @pot.last_watered = Time.now
+      authorize @list
+      @list.save
+      redirect_to list_path
+    end
+  
+    def edit
+    end
+  
+    def update
+      @list.update(list_params)
+    #   @pot.photos.attach(photos_params["photos"]) if photos_params["photos"]
+    #   if pot_params[:last_watered].present?
+    #     redirect_back fallback_location: pot_path(@pot)
+    #   else
+    #     redirect_to pot_path(@pot)
+    #   end
+    end
+  
+    def destroy
+      @list.destroy
+      redirect_to lists_path
+    end
+  
+    private
+  
+    # def set_pot
+    #   @pot = Pot.find(params[:id])
+    #   authorize @pot
+    # end
+  
+    def list_params
+      params.require(:list).permit(:name)
+    end
+  
+    # def photos_params
+    #   params.require(:pot).permit(photos: [])
+    # end 
 end
--- app/controllers/vocabulary_controller.rb
+++ app/controllers/vocabulary_controller.rb
@@ -1,2 +1,47 @@
 class VocabularyController < ApplicationController
+    before_action :set_word, only: [:show]
+
+  def index
+    # COMMupload img to cloudinary to create a url that can be called
+    # @words = Word.new
+    # @api_scan = params.dig(:plant, :image).present?
+    @words = policy_scope(Word).order(created_at: :desc)
+    # elsif params[:query].present?
+    #   @query = params[:query]
+    #   @plants = policy_scope(Plant).search_by_name(params[:query]).order(created_at: :desc)
+    # elsif @api_scan
+    #   api_call = ApiCall.new(params[:plant][:image])
+    #   # plant_search_name = TestHash["suggestions"].first["plant_name"]
+    #   @plant_search_name = api_call.call_api
+    #   @plants = policy_scope(Plant).search_by_name(@plant_search_name)
+    # end
+  end
+
+  def show
+    @word = Word.new
+  end
+
+  def filter
+    # @plant = Plant.new
+    # @water = params[:watering_schedule]
+    # @light = params[:light]
+    # if @water.present? && @light.present?
+    #   @plants = policy_scope(Plant).where(watering_schedule: @water, light:@light)
+    # elsif @water.present?
+    #   @plants = policy_scope(Plant).where(watering_schedule: @water)
+    # elsif @light.present?
+    #   @plants = policy_scope(Plant).where(light:@light)
+    #   @plants = policy_scope(Plant).where("watering_schedule LIKE ? AND light LIKE ?", @water, @light)
+    # else
+    # @plants = policy_scope(Plant).order(created_at: :asc)
+    # end
+    # render :index
+  end
+
+  private
+
+  def set_vocabulary
+    @word = Word.find(params[:id])
+    authorize @word
+  end
 end
