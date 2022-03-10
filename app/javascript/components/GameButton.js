cf7cb615a1f445732f3047b331f2dcc6b69205e5
Spencer Mattison <spenceramattison@gmail.com>
2022-03-10 15:52:24 +0900

    added react matching game to app

--- app/javascript/components/GameButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function GameButton() {
-    return(
-    <div>
-        <h1>Tango Typhoon</h1>
-    </div>
-    )
-}
--- app/javascript/components/List.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function List( props ) {
-  return (
-      <div className="list">
-        <p className="list--name">{props.name}</p>
-      </div>
-    )
-  }
