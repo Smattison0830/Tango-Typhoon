81168983491a18145c104050a4290a1488217058
spencer mattison <spenceramattison@gmail.com>
2022-03-19 12:19:35 +0900

    added react to card.index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_032658) do
+ActiveRecord::Schema.define(version: 2022_01_23_020249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
